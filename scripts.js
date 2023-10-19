a2abf33fda2b10c49889338eb4f764dc4ef27ab8
Ann Tech <anntech21@gmail.com>
2023-10-18 22:36:09 -0700

    incomplete

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
 //variables
 //HTML element Variables
-let screenVariable = document.querySelector(".screen");
+let screen = document.querySelector(".screen");
+let numbers = document.querySelectorAll(".number-button");
 
 //Variables for add, Subtract, multiple,divide,clear button,zero and equals sign
-
-let equals = document.querySelector("#equals");
 
 let add = document.querySelector("#add");
 
@@ -16,23 +15,19 @@ let divide = document.querySelector("#divide");
 
 let clear = document.querySelector(".clear-button");
 
+let equals = document.querySelector("#equals");
+
 let goBack = document.querySelector("#go-back");
 
 //array of elements for each button that has numbers 0-9:
-let numbers = document.querySelectorAll(".number-button");
+// let numbers = document.querySelectorAll(".number-button");
 
 //Variable for info from the user
-let valueOneFromUser = "";
-let valueTwoFromUser = "";
+let valueOne = "";
+let valueTwo = "";
 let currentOperator = "";
 
 //event listeners
-
-equals.addEventListener("click", function(){
-  console.log (doMath(valueOneFromUser, currentOperator, valueTwoFromUser));
- let answer = doMath(valueOneFromUser,currentOperator,valueTwoFromUser);
- screenVariable.textContent = answer;
-})
 
 //add event listeners to our operators (+,_,*,/)
 add.addEventListener("click", function () {
@@ -55,29 +50,64 @@ divide.addEventListener("click", function () {
   currentOperator = content;
 });
 
-//loop over the numbers array and for each item add an event listener whose function console.log("clicked");
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener("click", function () {
-    let content = numbers[i].textContent;
-    //console.log(numbers[i].textContent);
-    //if currentOperator ==="", update the value of valueOneFromUser = + content
-    if (currentOperator === "") {
-      valueOneFromUser += content;
-      console.log(valueOneFromUser, "FIRST number");
-      screenVariable.textContent = valueOneFromUser;
-    } else {
-      //add code to update valueTwoFromUser
-      valueTwoFromUser += content;
-      console.log(valueTwoFromUser, "SECOND number");
-      screenVariable.textContent = valueTwoFromUser;
-    }
-  });
-}
+equals.addEventListener("click", function () {
+  console.log(doMath(valueOne, currentOperator, valueTwo));
+  let answer = doMath(valueOne, currentOperator, valueTwo);
+  screen.textContent = answer;
+});
 
-//functions
+clear.addEventListener("click", function () {
+  valueOne = "";
+  currentOperator = "";
+  valueTwo = "";
+  screen.textContent = "0";
+});
+
+goBack.addEventListener("click", function () {
+  if (valueTwo.length > 1) {
+    valueTwo = valueTwo.slice(0, -1);
+    screen.textContent = valueTwo;
+  } else if (valueTwo.length === 1) {
+    valueTwo = "";
+    screen.textContent = "0";
+  } else if (currentOperator !== "") {
+    currentOperator = "";
+  } else if (valueOne.length > 1) {
+    valueOne = valueOne.slice(0, -1);
+    screen.textContent = valueOne;
+  } else if (valueOne.length === 1) {
+    valueOne = "";
+    screen.textContent = 0
+  };
+});
+
+//adding event listeners to all buttons that are numbers
+
+for (let i = 0; i < numbers.length; i++ ) {
+  numbers[i].addEventListener("click", function() {
+      let content = numbers[i].textContent; 
+      if (currentOperator === "") {
+      valueOne += content;
+      console.log(valueOne, "FIRST")
+      screen.textContent = valueOne
+  } else {
+      valueTwo += content;
+      console.log(valueTwo, "SECOND")
+     screen.textContent = valueTwo
+  }
+})
+};
+
+///functions 
 function doMath(value1, operator, value2) {
   //what value does the operator have?
   if (operator === "+") {
-    Number(value1) + Number(value2);
+    return Number(value1) + Number(value2);
+  } else if (operator === "-") {
+    return Number(value1) - Number(value2);
+  } else if (operator === "×") {
+    return Number(value1) * Number(value2);
+  } else if (operator === "+") {
+    return Number(value1) / Number(value2);
   }
 }
